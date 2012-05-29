eaeabd70bd348531b75597e4599d24d8d68473ee
Jeff Wintersinger <jeff.git@wintersinger.com>
2012-05-28 18:23:25 -0600

    Implement (faulty) base conversion algorithm.

--- QRTest/AppDelegate.m
+++ QRTest/AppDelegate.m
@@ -156,7 +156,7 @@
             // We're not currently in a range, so start a new one.
             insideRange = YES;
             // Set left-most bit to 1 to indicate that current partId starts new range.
-            partIdEncoded |= 0x80;
+            partIdEncoded |= 0x8000;
         } else {
             // We are either at the end of a range or outside a range entirely.
             insideRange = NO;
@@ -180,19 +180,77 @@
     return combined;
 }
 
+- (unsigned char*) encodePayload:(NSData*)payload {
+    // Taken directly from QR code specification, p. 21: http://raidenii.net/files/datasheets/misc/qr_code.pdf
+    unsigned char baseMapping[] = {
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+        'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+        'U', 'V', 'W', 'X', 'Y', 'Z',
+        ' ', '$', '%', '*', '+', '-', '.', '/', ':'
+    };
+    
+    NSUInteger payloadLength = [payload length];
+    const unsigned char* payloadBytes = [payload bytes];
+    //NSAssert([payload length] % 2 == 0, @"Payload length isn't even");
+    NSUInteger encodedLength = 3 * payloadLength / 2;
+    unsigned char* encoded = malloc(encodedLength + 1);
+    
+    uint16_t base = sizeof(baseMapping);
+    double baseLogInverse = 1 / log(base);
+    
+    for(int i = 0; i < payloadLength; i += sizeof(uint16_t)) {
+        printf("%hhx %hhx", *payloadBytes, *(payloadBytes + 1));    
+        uint16_t chunk = *(payloadBytes + i);
+        uint16_t exp = 0;
+       
+        while(true) {
+            // Take floor of log_base(chunk).
+            exp = (uint16_t)(log(chunk) * baseLogInverse);
+            uint16_t raisedBase = (uint16_t)pow(base, exp);
+            uint16_t mappingIndex = chunk / raisedBase;
+            *encoded = baseMapping[mappingIndex];
+            encoded++;
+            
+            chunk %= raisedBase;
+            if(exp == 0) break;
+        }
+    }
+    
+    encoded[encodedLength] = 0;
+    return encoded;
+}
+
 - (IBAction)generateQrCodeButtonPressed:(id)sender {
     NSString* input = [self.partIDsInput stringValue];
     NSData* payload = [self generatePayload:input];
     // Note that third paramemter to QRcode_encodeData specifies version number
     // (i.e., number of rows/columns in QR code). If equal to 0, as minimum value is chosen.
-    QRcode* resultCode = QRcode_encodeData([payload length], [payload bytes], 0, QR_ECLEVEL_L);
+    NSLog(@"Payload: %@ (%ld)", payload, [payload length]);
+    
+    for(int i = 0; i < [payload length]; i++) {
+        printf("%hhx ", *((unsigned char*)[payload bytes] + i));
+    }
+    printf("\n");
+    
+    unsigned char* happyPayload = malloc(4);
+    happyPayload[0] = 1;
+    happyPayload[1] = 2;
+    happyPayload[2] = 3;
+    happyPayload[3] = 4;
+    NSData* testPayload = [NSData dataWithBytes:happyPayload length:4];
+    NSLog(@"Test payload: %s", [self encodePayload:testPayload]);
+    
+    //QRcode* resultCode = QRcode_encodeData([payload length], (unsigned char*)[payload bytes], 0, QR_ECLEVEL_L);
+    /*QRcode* resultCode = QRcode_encodeString8bit(<#const char *string#>, <#int version#>, <#QRecLevel level#>)
     
     unsigned char* imageBuffer = NULL;
     unsigned int imageBufferWidth = 0;
     [self generateImageForQrCode:resultCode intoBuffer:&imageBuffer withWidth:&imageBufferWidth];
     [self displayImage:imageBuffer width:imageBufferWidth height:imageBufferWidth];
     
-    free(imageBuffer);
-    QRcode_free(resultCode);
+    // This memory should be freed, but corruption results when I do. Fix this when integrated into Presenter.
+    //free(imageBuffer);
+    QRcode_free(resultCode);*/
 }
 @end
